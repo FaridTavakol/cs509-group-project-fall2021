e02e584e7292594343dcbfb7a6bc374650697001
atifa <atifa_sarwar@hotmail.com>
2021-12-13 16:03:55 -0500

    Get Implementations by Algorithm IDs

---
+++ Proteus_AMS/src/main/java/edu/wpi/cs/proteus/demo/ImplementationByAlgoIDHandler.java
@@ -0,0 +1,45 @@
+package edu.wpi.cs.proteus.demo;
+
+import java.util.List;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+
+import edu.wpi.cs.proteus.db.ImplementationsDAO;
+import edu.wpi.cs.proteus.http.GetObjectsByID;
+import edu.wpi.cs.proteus.model.Implementation;
+
+public class ImplementationByAlgoIDHandler implements RequestHandler<Object, String> {
+
+	LambdaLogger logger;
+    @Override
+    public String handleRequest(Object input, Context context) {
+        context.getLogger().log("Input: " + input);
+
+//		AllImplementationsResponse response;
+//
+		Gson gson = new Gson();
+		GetObjectsByID instance = gson.fromJson(input.toString(), GetObjectsByID.class);
+//
+//		try {
+//			List<Implementation> pinstances = getImplementation(instance.getID());
+//					
+//			response = new AllImplementationsResponse(pinstances, 200);
+//		} catch (Exception e) {
+//			response = new AllImplementationsResponse(400, "Unable to get Implementations by Algo ID(" + e.getMessage() + ")");
+//		}
+		return instance.getID() ;
+	}
+
+	List<Implementation> getImplementation(String algoID) throws Exception {
+		if (logger != null) {
+			logger.log("in get Implementations");
+		}
+		ImplementationsDAO dao = new ImplementationsDAO();
+		System.out.println("connected to DB");
+
+		return dao.getAllImplementations(algoID);
+	}
+}
