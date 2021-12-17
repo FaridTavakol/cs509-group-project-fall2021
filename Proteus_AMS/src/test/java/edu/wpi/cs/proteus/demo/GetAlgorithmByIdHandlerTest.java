37ee8dc25339640900fc3f2fe70ffb8907aab6d9
FaridTavakol <ftavakkolmoghadd@wpi.edu>
2021-12-16 23:08:31 -0500

    Added GetAlgoByIDHandler Test.

--- Proteus_AMS/src/test/java/edu/wpi/cs/proteus/demo/GetAlgorithmByIdHandlerTest.java
+++ Proteus_AMS/src/test/java/edu/wpi/cs/proteus/demo/GetAlgorithmByIdHandlerTest.java
@@ -1,46 +1,52 @@
 package edu.wpi.cs.proteus.demo;
 
-import java.io.IOException;
-
-import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.amazonaws.services.lambda.runtime.Context;
 
+import edu.wpi.cs.proteus.db.AlgorithmsDAO;
 import edu.wpi.cs.proteus.http.GetAlgorithmByIdRequest;
 import edu.wpi.cs.proteus.http.GetAlgorithmByIdResponse;
+import edu.wpi.cs.proteus.model.Algorithm;
 
 /**
  * A simple test harness for locally invoking your Lambda function handler.
  */
 public class GetAlgorithmByIdHandlerTest {
 
-    private static GetAlgorithmByIdRequest input;
+	public GetAlgorithmByIdRequest input = new GetAlgorithmByIdRequest("4");
 
-    @BeforeClass
-    public static void createInput() throws IOException {
-        // TODO: set up your sample input object here.
-        input = null;
-    }
+//	@BeforeClass
+//	public static void createInput() throws IOException
+//	{
+//		// TODO: set up your sample input object here.
+//		input = null;
+//	}
 
-    private Context createContext() {
-        TestContext ctx = new TestContext();
+	private Context createContext()
+	{
+		TestContext ctx = new TestContext();
 
-        // TODO: customize your context here if needed.
-        ctx.setFunctionName("Your Function Name");
+		// TODO: customize your context here if needed.
+		ctx.setFunctionName("Your Function Name");
 
-        return ctx;
-    }
+		return ctx;
+	}
 
-    @Test
-    public void testGetAlgorithmByIdHandler() {
-        GetAlgorithmByIdHandler handler = new GetAlgorithmByIdHandler();
-        Context ctx = createContext();
+	@Test
+	public void testGetAlgorithmByIdHandler() throws Exception
+	{
+		AlgorithmsDAO dao = new AlgorithmsDAO();
+		Algorithm algo = new Algorithm("203", "1", "4");
+		dao.addAlgorithm(algo);
 
-        GetAlgorithmByIdResponse output = handler.handleRequest(input, ctx);
+		GetAlgorithmByIdHandler handler = new GetAlgorithmByIdHandler();
+		Context ctx = createContext();
+//		input.setID("1");
+		GetAlgorithmByIdResponse output = handler.handleRequest(input, ctx);
 
-        // TODO: validate output here if needed.
-        Assert.assertEquals("Hello from Lambda!", output);
-    }
+		// TODO: validate output here if needed.
+		String ID = output.algorithmId;
+
+	}
 }
